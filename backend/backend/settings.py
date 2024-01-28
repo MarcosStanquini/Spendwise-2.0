d4ea029c93036d0a292d931d625d09827b3570b1
eualbino <lucas.g.apostolo@gmail.com>
2024-01-28 14:30:10 -0300

    Mudança na porta e permission class

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -114,7 +114,7 @@ REST_FRAMEWORK = {
 
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
+    "http://localhost:8080",
 
 ]
 
--- backend/budget_api/views.py
+++ backend/budget_api/views.py
@@ -4,7 +4,7 @@ from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 
 class BudgetViewSet(viewsets.ModelViewSet):
-    #permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated,)
     queryset = Budget.objects.all()
     serializer_class = BudgetSerializer
 
