f35fc971696b1dbbf8dc18e6b57bec1b24ef4a83
MarcosStanquini <marcossnarquini@gmail.com>
2024-02-06 10:28:09 -0300

    feat: add budget tests

--- backend/conftest.py
+++ backend/conftest.py
@@ -1,13 +1,8 @@
 
 import pytest
 from users.models import User
-
-"""@pytest.fixture(scope = 'session')
-def user_test():
-    user = User.objects.create(name='Test', username='test@gmail.com')
-    print(user)
-    return  user"""
-
+from datetime import date
+from budget_api.models import Budget
 
 @pytest.fixture
 def new_user_factory(db):
@@ -16,7 +11,7 @@ def new_user_factory(db):
         username: str = "test@gmail.com",
         password: str = "test",
     ):
-        user = User.objects.create_user(
+        user = User.objects.create(
             name = name,
             username = username,
             password = password
@@ -29,4 +24,35 @@ def new_user_factory(db):
 def new_user(db, new_user_factory):
     return new_user_factory("Test", "test@gmail.com", "test")
 
-    
+
+@pytest.fixture
+def new_budget_factory(db, new_user):
+    def create_app_budget(
+        name: str = "Test_Budget",
+        value: int = "200",
+        date: date = date(2024, 2, 6),
+        description: str = "Gastos",
+        expense: bool = True,
+        user_id = new_user.id
+    ):
+        budget = Budget.objects.create(
+        name = name, 
+        value = value,
+        date = date,
+        description = description,
+        expense = expense,
+        user_id = user_id
+        )
+        return budget
+    return create_app_budget
+
+@pytest.fixture
+def new_budget(db, new_budget_factory, new_user):
+    return new_budget_factory(
+        name="Test_Budget",
+        value=200,
+        date=date(2024, 2, 6),
+        description="Gastos",
+        expense=True,
+        user_id=new_user.id
+    )
--- backend/tests/bugdet_api_test.py
+++
@@ -1,4 +0,0 @@
-import pytest
-
-def test_example():
-    assert 1 == 1
